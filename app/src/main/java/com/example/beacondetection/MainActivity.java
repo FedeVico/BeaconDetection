c4ae1570d4efda0bd71bc8496ff84d41efdbfb70
Federico Gonzalez Vico <f.gonzalez.vico@avanade.com>
2024-03-07 20:34:17 +0100

    esto no peta al pedir permisos y escanea pero no encuentra nada

--- app/src/main/java/com/example/beacondetection/MainActivity.java
+++ app/src/main/java/com/example/beacondetection/MainActivity.java
@@ -1,182 +1,366 @@
 package com.example.beacondetection;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.RemoteException;
+import android.provider.Settings;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
-import android.Manifest;
+import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
 import org.altbeacon.beacon.Beacon;
 import org.altbeacon.beacon.BeaconConsumer;
 import org.altbeacon.beacon.BeaconManager;
 import org.altbeacon.beacon.BeaconParser;
 import org.altbeacon.beacon.Identifier;
-import org.altbeacon.beacon.MonitorNotifier;
 import org.altbeacon.beacon.RangeNotifier;
 import org.altbeacon.beacon.Region;
+
+import java.util.ArrayList;
 import java.util.Collection;
 
-public class MainActivity extends AppCompatActivity implements BeaconConsumer {
-    protected static final String TAG = "BeaconsMonitoring";
-    private BeaconManager beaconManager = null;
-    private Region beaconRegion = null;
-    private Button startButton;
-    private Button stopButton;
-    private static final String ALTBEACON_LAYOUT = "m:2-3=beac,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25";
-    private void showAlert(final String title, final String message) {
-        runOnUiThread(() -> {
-            AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
-            alertDialog.setTitle(title);
-            alertDialog.setMessage(message);
-            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
-                    (dialog, which) -> {
-                        dialog.dismiss();
-                    });
-            alertDialog.show();
-        });
-    }
-    @RequiresApi(api = Build.VERSION_CODES.S)
+/**
+ * App main activity, gestiona la detección de beacons, mostrando un mensaje de los beacons
+ * detectados
+ *
+ * @author David González Verdugo
+ */
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, BeaconConsumer,
+        RangeNotifier {
+
+    protected final String TAG = MainActivity.this.getClass().getSimpleName();
+    ;
+    private static final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
+    private static final int REQUEST_ENABLE_BLUETOOTH = 1;
+    private static final long DEFAULT_SCAN_PERIOD_MS = 6000l;
+    private static final String ALL_BEACONS_REGION = "AllBeaconsRegion";
+
+    // Para interactuar con los beacons desde una actividad
+    private BeaconManager mBeaconManager;
+
+    // Representa el criterio de campos con los que buscar beacons
+    private Region mRegion;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.d(TAG, "onCreate called");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startButton = (Button) findViewById(R.id.scanButton);
-        stopButton = (Button) findViewById(R.id.stopButton);
-        startButton.setOnClickListener((v) ->{startBeaconMonitoring();});
-        stopButton.setOnClickListener((v) ->{stopBeaconMonitoring();});
-        checkBluetoothPermissions();
-        beaconManager = BeaconManager.getInstanceForApplication(this);
-        beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout(ALTBEACON_LAYOUT));
-        beaconManager.bind(this);
-    }
-    private static final int REQUEST_PERMISSION_BLUETOOTH = 1;
 
-    @RequiresApi(api = Build.VERSION_CODES.S)
-    private boolean checkBluetoothPermissions() {
-        boolean permissionsGranted = true;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            int bluetoothScanPermissionCheck = ContextCompat.checkSelfPermission(this,
-                    Manifest.permission.BLUETOOTH_SCAN);
-            if (bluetoothScanPermissionCheck != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.BLUETOOTH_SCAN},
-                        1004);
-                permissionsGranted = false;
+        getStartButton().setOnClickListener(this);
+        getStopButton().setOnClickListener(this);
+
+        mBeaconManager = BeaconManager.getInstanceForApplication(this);
+
+        // Fijar un protocolo beacon, Eddystone en este caso
+        mBeaconManager.getBeaconParsers().add(new BeaconParser().
+                setBeaconLayout(BeaconParser.EDDYSTONE_UID_LAYOUT));
+
+        ArrayList<Identifier> identifiers = new ArrayList<>();
+
+        mRegion = new Region(ALL_BEACONS_REGION, identifiers);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        if (view.equals(findViewById(R.id.startReadingBeaconsButton))) {
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+                // Si los permisos de localización todavía no se han concedido, solicitarlos
+                if (this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                        PackageManager.PERMISSION_GRANTED) {
+
+                    askForLocationPermissions();
+
+                } else { // Permisos de localización concedidos
+
+                    prepareDetection();
+                }
+
+            } else { // Versiones de Android < 6
+
+                prepareDetection();
+            }
+
+        } else if (view.equals(findViewById(R.id.stopReadingBeaconsButton))) {
+
+            stopDetectingBeacons();
+
+            /*BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+            // Desactivar bluetooth
+            if (mBluetoothAdapter.isEnabled()) {
+                mBluetoothAdapter.disable();
+            }*/
+        }
+    }
+
+    /**
+     * Activar localización y bluetooth para empezar a detectar beacons
+     */
+    private void prepareDetection() {
+
+        if (!isLocationEnabled()) {
+
+            askToTurnOnLocation();
+
+        } else { // Localización activada, comprobemos el bluetooth
+
+            BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+            if (mBluetoothAdapter == null) {
+
+                showToastMessage(getString(R.string.not_support_bluetooth_msg));
+
+            } else if (mBluetoothAdapter.isEnabled()) {
+
+                startDetectingBeacons();
+
+            } else {
+
+                // Pedir al usuario que active el bluetooth
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BLUETOOTH);
             }
         }
-        int bluetoothPermissionCheck = ContextCompat.checkSelfPermission(this,
-                Manifest.permission.BLUETOOTH);
-        int bluetoothAdminPermissionCheck = ContextCompat.checkSelfPermission(this,
-                Manifest.permission.BLUETOOTH_ADMIN);
-        int coarseLocationPermissionCheck = ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_COARSE_LOCATION);
-
-        if (bluetoothPermissionCheck != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.BLUETOOTH},
-                    1001);
-            permissionsGranted = false;
-        }
-
-        if (bluetoothAdminPermissionCheck != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.BLUETOOTH_ADMIN},
-                    1002);
-            permissionsGranted = false;
-        }
-
-        if (coarseLocationPermissionCheck != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
-                    1003);
-            permissionsGranted = false;
-        }
-
-        return permissionsGranted;
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
-    private Boolean entryMessage = false;
-    private Boolean exitMessage = false;
-    private Boolean rangingMessage = false;
+        if (requestCode == REQUEST_ENABLE_BLUETOOTH) {
 
-    private void stopBeaconMonitoring() {
-        try {
-            beaconManager.stopMonitoringBeaconsInRegion(beaconRegion);
-            beaconManager.stopRangingBeaconsInRegion(beaconRegion);
-        } catch (RemoteException e) {
-            e.printStackTrace();
+            // Usuario ha activado el bluetooth
+            if (resultCode == RESULT_OK) {
+
+                startDetectingBeacons();
+
+            } else if (resultCode == RESULT_CANCELED) { // User refuses to enable bluetooth
+
+                showToastMessage(getString(R.string.no_bluetooth_msg));
+            }
         }
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
-    private void startBeaconMonitoring() {
-        Log.d(TAG, "startBeaconMonitoring called");
-        try {
-            beaconRegion = new Region("MyBeacons", Identifier.parse("4d6fc88b-be75-6698-da48-6866a36ec78e"),
-                    Identifier.parse("4"), Identifier.parse("200"));
-            beaconManager.startMonitoringBeaconsInRegion(beaconRegion);
-            beaconManager.startRangingBeaconsInRegion(beaconRegion);
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        }
+    /**
+     * Empezar a detectar los beacons, ocultando o mostrando los botones correspondientes
+     */
+    private void startDetectingBeacons() {
 
+        // Fijar un periodo de escaneo
+        mBeaconManager.setForegroundScanPeriod(DEFAULT_SCAN_PERIOD_MS);
+
+        // Enlazar al servicio de beacons. Obtiene un callback cuando esté listo para ser usado
+        mBeaconManager.bind(this);
+
+        // Desactivar botón de comenzar
+        getStartButton().setEnabled(false);
+        getStartButton().setAlpha(.5f);
+
+        // Activar botón de parar
+        getStopButton().setEnabled(true);
+        getStopButton().setAlpha(1);
     }
 
     @Override
     public void onBeaconServiceConnect() {
-        Log.d(TAG, "onBeaconServiceConnect called");
-        beaconManager.setMonitorNotifier(new MonitorNotifier() {
+
+        try {
+            // Empezar a buscar los beacons que encajen con el el objeto Región pasado, incluyendo
+            // actualizaciones en la distancia estimada
+            mBeaconManager.startRangingBeaconsInRegion(mRegion);
+
+            showToastMessage(getString(R.string.start_looking_for_beacons));
+
+        } catch (RemoteException e) {
+            Log.d(TAG, "Se ha producido una excepción al empezar a buscar beacons " + e.getMessage());
+        }
+
+        mBeaconManager.addRangeNotifier(this);
+    }
 
 
-            public void didEnterRegion(Region region) {
-                if (!entryMessage) {
-                    showAlert("didEnterRegion", "Entering region " + region.getUniqueId() +
-                            " Beacon detected UUID/major/minor: " + region.getId1() +
-                            "/" + region.getId2() + "/" + region.getId3());
-                    entryMessage = true;
-                }
-            }
+    /**
+     * Método llamado cada DEFAULT_SCAN_PERIOD_MS segundos con los beacons detectados durante ese
+     * periodo
+     */
+    @SuppressLint("StringFormatInvalid")
+    @Override
+    public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
 
+        if (beacons.size() == 0) {
+            showToastMessage(getString(R.string.no_beacons_detected));
+        }
 
-            public void didExitRegion(Region region) {
-                if (!exitMessage) {
-                    showAlert("didExitRegion", "Exiting region " + region.getUniqueId() +
-                            " Beacon detected UVID/major/minor: " + region.getId1() +
-                            "/" + region.getId2() + "/" + region.getId3());
-                    exitMessage = true;
-                }
-            }
+        for (Beacon beacon : beacons) {
+            showToastMessage(getString(R.string.beacon_detected, beacon.getId3()));
+        }
+    }
 
-            public void didDetermineStateForRegion(int i, Region region) {
+    private void stopDetectingBeacons() {
+
+        try {
+            mBeaconManager.stopMonitoringBeaconsInRegion(mRegion);
+            showToastMessage(getString(R.string.stop_looking_for_beacons));
+        } catch (RemoteException e) {
+            Log.d(TAG, "Se ha producido una excepción al parar de buscar beacons " + e.getMessage());
+        }
+
+        mBeaconManager.removeAllRangeNotifiers();
+
+        // Desenlazar servicio de beacons
+        mBeaconManager.unbind(this);
+
+        // Activar botón de comenzar
+        getStartButton().setEnabled(true);
+        getStartButton().setAlpha(1);
+
+        // Desactivar botón de parar
+        getStopButton().setEnabled(false);
+        getStopButton().setAlpha(.5f);
+    }
+
+    /**
+     * Comprobar permisión de localización para Android >= M
+     */
+    private void askForLocationPermissions() {
+
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.location_access_needed);
+        builder.setMessage(R.string.grant_location_access);
+        builder.setPositiveButton(android.R.string.ok, null);
+        builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            public void onDismiss(DialogInterface dialog) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                        PERMISSION_REQUEST_COARSE_LOCATION);
             }
         });
+        builder.show();
+    }
 
-        beaconManager.setRangeNotifier(new RangeNotifier() {
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case PERMISSION_REQUEST_COARSE_LOCATION: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    prepareDetection();
+                } else {
+                    final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle(R.string.funcionality_limited);
+                    builder.setMessage(getString(R.string.location_not_granted) +
+                            getString(R.string.cannot_discover_beacons));
+                    builder.setPositiveButton(android.R.string.ok, null);
+                    builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                        @Override
+                        public void onDismiss(DialogInterface dialog) {
+
+                        }
+                    });
+                    builder.show();
+                }
+                return;
+            }
+        }
+    }
+
+    /**
+     * Comprobar si la localización está activada
+     *
+     * @return true si la localización esta activada, false en caso contrario
+     */
+    private boolean isLocationEnabled() {
+
+        LocationManager lm = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        boolean networkLocationEnabled = false;
+
+        boolean gpsLocationEnabled = false;
+
+        try {
+            networkLocationEnabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+            gpsLocationEnabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        } catch (Exception ex) {
+            Log.d(TAG, "Excepción al obtener información de localización");
+        }
+
+        return networkLocationEnabled || gpsLocationEnabled;
+    }
+
+    /**
+     * Abrir ajustes de localización para que el usuario pueda activar los servicios de localización
+     */
+    private void askToTurnOnLocation() {
+
+        // Notificar al usuario
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setMessage(R.string.location_disabled);
+        dialog.setPositiveButton(R.string.location_settings, new DialogInterface.OnClickListener() {
             @Override
-
-            public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
-                if (!rangingMessage && beacons != null && !beacons.isEmpty()) {
-                    for (Beacon beacon : beacons) {
-                        showAlert("didExitRegion", "Ranging region" + region.getUniqueId() +
-                                " Beacon detected UUID/major/minor: " + beacon.getId1() +
-                                "/" + beacon.getId2() + "/" + beacon.getId3());
-
-                    }
-                    rangingMessage = true;
-                }
+            public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                // TODO Auto-generated method stub
+                Intent myIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(myIntent);
             }
         });
+        dialog.show();
+    }
+
+    private Button getStartButton() {
+        return (Button) findViewById(R.id.startReadingBeaconsButton);
+    }
+
+    private Button getStopButton() {
+        return (Button) findViewById(R.id.stopReadingBeaconsButton);
+    }
+
+    /**
+     * Mostrar mensaje
+     *
+     * @param message mensaje a enseñar
+     */
+    private void showToastMessage (String message) {
+        Toast toast = Toast.makeText(this, message, Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mBeaconManager.removeAllRangeNotifiers();
+        mBeaconManager.unbind(this);
     }
 }
-
